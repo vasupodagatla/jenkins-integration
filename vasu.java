7c8455c6d53b2445fd0187742ea15f7394abd03c
vasupodagatla <102164016+vasupodagatla@users.noreply.github.com>
2023-05-17 13:56:07 +0530

    added deletefile

---
+++ vasu.java
@@ -0,0 +1,17 @@
+// Java program to delete a file
+import java.io.*;
+
+public class Test {
+	public static void main(String[] args)
+	{
+		File file
+			= new File("C:\\Users\\Mayank\\Desktop\\1.txt");
+
+		if (file.delete()) {
+			System.out.println("File deleted successfully");
+		}
+		else {
+			System.out.println("Failed to delete the file");
+		}
+	}
+}
